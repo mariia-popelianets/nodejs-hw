61435fc94c4e04953ae138a29511e8f94c4c4dbb
Mariia Popelianets <mariakh0128@gmail.com>
2026-09-06 16:58:42 +0300

    fix: handle express errors correctly

--- src/server.js
+++ src/server.js
@@ -47,7 +47,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   res.status(500).json({
     message: err.message,
   });
