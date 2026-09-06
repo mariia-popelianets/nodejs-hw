3204f39cf3f96ede542cff12acbc81c2dec146e0
Mariia Popelianets <mariakh0128@gmail.com>
2026-09-06 16:48:06 +0300

    feat: add express server

---
+++ src/server.js
@@ -0,0 +1,58 @@
+import express from 'express';
+
+import cors from 'cors';
+import 'dotenv/config';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+//GET /test-error
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+//GET /notes
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+//GET /notes/:noteId
+app.get('/notes/:noteId', (req, res) => {
+  const idParam = req.params.noteId;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${idParam}`,
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
